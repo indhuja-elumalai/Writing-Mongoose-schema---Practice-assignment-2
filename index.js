b6481a48888c36493d9a3a0f598189ae8bb1f731
indhuja-elumalai <indhuja1012006@gmail.com>
2025-01-28 14:14:06 +0530

    committed

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const { resolve } = require('path');
+const BlogPost = require('./schema');
 
 const app = express();
-const port = 3010;
+const port = 3000;
 
 app.use(express.static('static'));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
